c641cb97d021895f0386034f1c642901b761b438
Dany Haddad <danyhaddad43@gmail.com>
2018-02-22 19:45:43 -0600

    add proof for mutual information submodularity

--- auto/scribe.el
+++ auto/scribe.el
@@ -3,6 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "twoside")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm2e" "ruled" "vlined")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,19 +21,28 @@
     "graphicx"
     "amsthm"
     "amssymb"
-    "hyperref")
+    "hyperref"
+    "algorithm2e"
+    "cancel")
    (TeX-add-symbols
     "endrefs"
     '("fig" 3)
     '("lecture" 4)
     "QEDB"
     "xor"
+    "indep"
     "E"
+    "argmax"
     "beginrefs"
     "bibentry")
    (LaTeX-add-labels
+    "eq:1"
     "fig:ugm")
    (LaTeX-add-environments
+    '("subproof" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-counters
+    "lecnum")
+   (LaTeX-add-amsthm-newtheorems
     "theorem"
     "example"
     "lemma"
@@ -32,8 +50,7 @@
     "claim"
     "corollary"
     "note"
-    "definition")
-   (LaTeX-add-counters
-    "lecnum"))
+    "definition"
+    "problem"))
  :latex)
 
