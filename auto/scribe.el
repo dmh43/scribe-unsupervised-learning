a5efa78599e384f30a6914099b2431759abf81b7
Dany Haddad <danyhaddad43@gmail.com>
2018-03-28 15:45:08 -0500

    update ground set symbol

--- auto/scribe.el
+++ auto/scribe.el
@@ -5,12 +5,12 @@
                      '(("article" "twoside")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("algorithm2e" "ruled" "vlined")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,6 +33,8 @@
     "indep"
     "E"
     "argmax"
+    "spbigvert"
+    "spvert"
     "beginrefs"
     "bibentry")
    (LaTeX-add-labels
